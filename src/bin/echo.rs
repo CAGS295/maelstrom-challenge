2647fb086401cdf6c588d90b0041fbb3ab683667
Carlos Alejandro Gutierrez Sandoval <calgusa@gmail.com>
2024-03-15 00:41:59 -0600

    move Node

--- src/bin/echo.rs
+++ src/bin/echo.rs
@@ -1,38 +1,8 @@
-use maelstrom_challenge::Body;
-use maelstrom_challenge::Extra;
 use std::io::stdin;
 use std::io::stdout;
 use std::io::BufRead;
-use std::io::Write;
 
-use maelstrom_challenge::Message;
-
-struct Node {}
-
-impl Node {
-    //TODO generic reader writer;
-    fn init<W: Write>(mut writer: W) {
-        let stdin = stdin().lock();
-        let line = stdin
-            .lines()
-            .into_iter()
-            .next()
-            .expect("Init msg always present")
-            .unwrap();
-        let msg: Message = serde_json::from_str(&line).unwrap();
-        serde_json::to_writer(
-            &mut writer,
-            &msg.reply(Body {
-                msg_id: 0,
-                extra: Extra::InitOk {
-                    in_reply_to: msg.body.msg_id,
-                },
-            }),
-        )
-        .unwrap();
-        writer.write_all(b"\n").unwrap();
-    }
-}
+use maelstrom_challenge::Node;
 
 fn main() {
     Node::init(stdout().lock());
--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,7 @@
 use serde::{Deserialize, Serialize};
+use std::io::stdin;
+use std::io::BufRead;
+use std::io::Write;
 
 #[derive(Deserialize, Debug, Serialize, Clone)]
 pub struct Message {
@@ -38,6 +41,33 @@ pub enum Extra {
     InitOk {
         in_reply_to: u64,
     },
+}
+
+pub struct Node {}
+
+impl Node {
+    //TODO generic reader writer;
+    pub fn init<W: Write>(mut writer: W) {
+        let stdin = stdin().lock();
+        let line = stdin
+            .lines()
+            .into_iter()
+            .next()
+            .expect("Init msg always present")
+            .unwrap();
+        let msg: Message = serde_json::from_str(&line).unwrap();
+        serde_json::to_writer(
+            &mut writer,
+            &msg.reply(Body {
+                msg_id: 0,
+                extra: Extra::InitOk {
+                    in_reply_to: msg.body.msg_id,
+                },
+            }),
+        )
+        .unwrap();
+        writer.write_all(b"\n").unwrap();
+    }
 }
 
 #[cfg(test)]
