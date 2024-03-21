b8ce5bddcecb60176d9ab01e67113fa5d1197db6
Carlos Alejandro Gutierrez Sandoval <calgusa@gmail.com>
2024-03-21 16:37:33 -0600

    unique-id test

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,8 +1,15 @@
 #!/usr/bin/env bash
+set -ueo > /dev/null
 
 case $1 in 
     echo)
     cargo build && ../maelstrom/maelstrom test -w echo --bin target/debug/echo --node-count 1 --time-limit 3
+    ;;
+    unique-id)
+    cargo build && ../maelstrom/maelstrom test -w unique-ids --bin target/debug/unique_id --node-count 1 --time-limit 3
+    ;;
+    *)
+    echo $1 not supported
     ;;
 esac
 
