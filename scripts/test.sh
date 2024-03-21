437308717a32459e2895aa15afbcf93d0a19950c
Carlos Alejandro Gutierrez Sandoval <calgusa@gmail.com>
2024-03-21 17:54:40 -0600

    bump node count for all tests

--- scripts/test.sh
+++ scripts/test.sh
@@ -3,10 +3,10 @@ set -ueo > /dev/null
 
 case $1 in 
     echo)
-    cargo build && ../maelstrom/maelstrom test -w echo --bin target/debug/echo --node-count 1 --time-limit 3
+    cargo build && ../maelstrom/maelstrom test -w echo --bin target/debug/echo --node-count 2 --time-limit 3
     ;;
     unique-id)
-    cargo build && ../maelstrom/maelstrom test -w unique-ids --bin target/debug/unique_id --node-count 1 --time-limit 3
+    cargo build && ../maelstrom/maelstrom test -w unique-ids --bin target/debug/unique_id --node-count 2 --time-limit 3
     ;;
     *)
     echo $1 not supported
