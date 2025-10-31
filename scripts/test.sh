b985059aa8525d4697176e5f6750673d38714eeb
carlos alejandro gutierrez sandoval <calgusa@gmail.com>
2025-10-31 14:14:43 -0600

    maelstrom in subfolder

--- scripts/test.sh
+++ scripts/test.sh
@@ -3,13 +3,13 @@ set -ueo > /dev/null
 
 case $1 in 
     echo)
-    cargo build && ../maelstrom/maelstrom test -w echo --bin target/debug/echo --node-count 2 --time-limit 3
+    cargo build && ./maelstrom/maelstrom test -w echo --bin target/debug/echo --node-count 2 --time-limit 3
     ;;
     unique-id)
-    cargo build && ../maelstrom/maelstrom test -w unique-ids --bin target/debug/unique_id --node-count 2 --time-limit 3
+    cargo build && ./maelstrom/maelstrom test -w unique-ids --bin target/debug/unique_id --node-count 2 --time-limit 3
     ;;
     broadcast)
-    cargo build && ../maelstrom/maelstrom test -w broadcast --bin target/debug/broadcast --node-count 5 --time-limit 5 --rate 10
+    cargo build && ./maelstrom/maelstrom test -w broadcast --bin target/debug/broadcast --node-count 5 --time-limit 5 --rate 10
     ;;
     *)
     echo $1 not supported
